efdc5a087f0ee8fc8b64e6d39adb098815d7ddea
mfile <matyas.file@nokia.com>
2018-07-06 09:37:32 +0200

    system environment

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main{
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		String sysEnvStr = System.getenv("string_parameter");
+		System.out.println("String parameter value : " + sysEnvStr);
 	}
 }
